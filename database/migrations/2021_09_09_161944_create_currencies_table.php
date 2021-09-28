8b061bcd7411c7464d0e85b4a28a87ca099b4ab3
Daniel Weiser <danielweisercs@gmail.com>
2021-09-28 16:09:28 +0300

    init project

---
+++ database/migrations/2021_09_09_161944_create_currencies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('currency');
+            $table->float('usd');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
